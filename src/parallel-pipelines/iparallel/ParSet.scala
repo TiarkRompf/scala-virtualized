70658576b0993839e18ea96f27421807ff581792
Philipp Haller <hallerp@gmail.com>
2011-04-13 16:56:53 +0200

    Building simple pipelines out of mapped ParSets. Added simple map fusion optimization.

--- src/parallel-pipelines/iparallel/ParSet.scala
+++ src/parallel-pipelines/iparallel/ParSet.scala
@@ -18,48 +18,79 @@ object ParSet {
 
 }
 
+trait Forceable[T] 
+
 trait ParSet[T] extends ParIterable[T] {
 
   def map[U](f: T => U)(implicit s: SourceLocation): ParSet[U]
 
   def force(): ParSet[T]
 
+  def foreach[U](f: T => U): Unit
+
+  override def toString(): String = {
+    var elems: List[T] = List()
+    foreach(elems ::= _)
+    "ParSet(" + elems.mkString(",") + ")"
+  }
+
 }
 
-class ParSetImpl[T](elements: immutable.Set[T]) extends ParSet[T] { self =>
+/* A node representing a map operation in an execution graph.
+ * Here, `input` may be another `MapNode` which produces a `ParSet`, or
+ * a materialized or non-materialized `ParSet`.
+ */
+class MapNode[T, U](val input: ParSet[T], val f: T => U) extends ParSet[U] {
+  var forced: Option[ParSetImpl[U]] = None
 
-  class MapNode[U](f: T => U) extends ParSet[U] {
-    var forced: Option[ParSetImpl[U]] = None
-
-    // the actual implementation of a parallel map
-    def force(): ParSet[U] = {
-      // split set into parts
-      // TODO: take #processors into account or do adaptive work stealing
-      val futures = for (splitter <- self.parallelIterator.split) yield
-        future {
-          // split task further
-          for (s <- splitter.split; elem <- s) yield
-            f(elem)
-        }
-      var newElems = immutable.Set.empty[U]
-      for (fut <- futures) {
-        newElems ++= fut()
+  // the actual implementation of a parallel map
+  def force(): ParSet[U] = {
+    // split set into parts
+    // TODO: take #processors into account or do adaptive work stealing
+    val futures = for (splitter <- input.parallelIterator.split) yield
+      future {
+        // split task further
+        for (s <- splitter.split; elem <- s) yield
+          f(elem)
       }
-      val impl = new ParSetImpl(newElems)
-      forced = Some(impl)
-      impl
+    var newElems = immutable.Set.empty[U]
+    for (fut <- futures) {
+      newElems ++= fut()
     }
-
-    def getOrForce: ParSet[U] =
-      if (forced.isEmpty) force()
-      else forced.get
-
-    def parallelIterator: Splitter[U] =
-      getOrForce.parallelIterator
-
-    def map[V](f: U => V)(implicit s: SourceLocation): ParSet[V] =
-      getOrForce.map(f)
+    val impl = new ParSetImpl(newElems)
+    forced = Some(impl)
+    impl
   }
+
+  def getOrForce: ParSet[U] =
+    if (forced.isEmpty) force()
+    else forced.get
+
+  def parallelIterator: Splitter[U] =
+    getOrForce.parallelIterator
+
+  def map[V](g: U => V)(implicit s: SourceLocation): ParSet[V] = {
+    // Instead of forcing the first set, we build an execution graph out of
+    // MapNodes.
+    //getOrForce.map(f)
+    new MapNode(this, g)
+  }
+
+  def foreach[V](g: U => V): Unit =
+    getOrForce.foreach(g)
+
+  // Implements map fusion.
+  def optimized: ParSet[U] = {
+    input match {
+      case inputNode: MapNode[s, T] =>
+        new MapNode(inputNode.input, inputNode.f andThen this.f)
+      case _ =>
+        this
+    }
+  }
+}
+
+class ParSetImpl[T](elements: immutable.Set[T]) extends ParSet[T] {
 
   def force(): ParSet[T] =
     this
@@ -85,7 +116,10 @@ class ParSetImpl[T](elements: immutable.Set[T]) extends ParSet[T] { self =>
   }
 
   def map[U](f: T => U)(implicit s: SourceLocation): ParSet[U] =
-    new MapNode(f)
+    new MapNode(this, f)
+
+  def foreach[U](f: T => U): Unit =
+    elements.foreach(f)
 
 /*
   def newCombiner = new Combiner[T] {
--- src/parallel-pipelines/iparallel/Pipeline.scala
+++ src/parallel-pipelines/iparallel/Pipeline.scala
@@ -19,7 +19,15 @@ object Pipeline {
     val set = ParSet(immutable.Set(1, 2, 3, 4))
     val delayed = set map { elem => println(elem); elem + 1 }
     println("now forcing delayed mapped set")
-    delayed.force()
+    val forcedSet = delayed.force()
+    println(forcedSet)
+
+    val set2 = ParSet(immutable.Set(10, 20, 30, 40))
+    val set3 = set2 map { elem => println(elem); elem + 1 }
+    val set4 = set3 map { elem => println(elem); elem + 1 }
+    println("now forcing doubly-mapped set")
+    val forcedSet4 = set4.force()
+    println(forcedSet4)
 
     val map = ParMap(immutable.Set((1, 1), (2, 2), (3, 3), (4, 4)))
     val res = map foreach { (l, r) => println(l + r) }
